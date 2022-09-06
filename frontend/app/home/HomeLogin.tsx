8c5d1d8c63c9054be45a4983cf9151c49c910ce4
Mackintac <mack-robinson@hotmail.ca>
2022-09-05 20:20:26 -0400

    end of quick session commit

--- frontend/app/home/HomeLogin.tsx
+++ frontend/app/home/HomeLogin.tsx
@@ -2,12 +2,25 @@ export function HomeLogin() {
   return (
     <>
       <div className='home__LoginForm'>
-        <button>Login</button>
-        <button>Check login</button>
-        <button>Check Game</button>
-        <button>Join Game</button>
-        <button>Leave Game</button>
-        <button>Logout</button>
+        <div className='home_LoginForm-field'>
+          <button>Login</button>
+        </div>
+
+        <div className='home_LoginForm-field'>
+          <button>Logout</button>
+        </div>
+        <div className='home_LoginForm-field'>
+          <button>checklogin</button>
+        </div>
+        <div className='home_LoginForm-field'>
+          <button>checkgame</button>
+        </div>
+        <div className='home_LoginForm-field'>
+          <button>joingame</button>
+        </div>
+        <div className='home_LoginForm-field'>
+          <button>leavegame</button>
+        </div>
       </div>
     </>
   );
